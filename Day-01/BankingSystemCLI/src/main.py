08dbdf2e905bfb5410bc99cc0098de2508aebc44
MALHAR BHATT <malharbhatt.devs@gmail.com>
2026-02-19 13:40:37 +0530

    PHASE 1.3 : redeveloping in new desktop.

--- Day-01/BankingSystemCLI/src/main.py
+++ Day-01/BankingSystemCLI/src/main.py
@@ -3,17 +3,18 @@ from BankingSystem import *
 class main(Account,BankingSystem):
     print('Welcome to CLI formatted Banking System !')
     print('Please Choice from the below options :')
-    menu ="""====Main Menu===
-        1.Login Account
-        2.Register Account
-        3.Exit"""
-    choiceM = int(input(menu))
+    print("""====Main Menu====
+    1.Login Account
+    2.Register Account
+    3.Exit
+=================""")
+    choiceM = int(input('Enter the Choice here ->'))
     if choiceM == 1 :
         print('Entering the Login System.')
-        # super.login()
+        super.login()
     elif choiceM == 2 :
         print('Entering the Registering System.')
-        # super.registerSystem()
+        super.registerSystem()
     elif choiceM == 3 :
         print('''Exiting the Banking System.
                  Thank You for your time.''')
